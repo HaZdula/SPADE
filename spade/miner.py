5d6c1a2ef231737f3e451a2c7b1de2c114ce9d3d
Hanna Zdulska <zdula616@gmail.com>
2022-06-07 01:04:20 +0200

    add spade implementation

--- spade/miner.py
+++ spade/miner.py
@@ -2,23 +2,48 @@ from collections import defaultdict
 from utils import *
 import argparse, sys
 
-def mine(df, min_support):
+def mine(df, min_support, max_depth):
     # df with columns Id, Date, Products
-    # @todo uogolnic zeby dzialalo moze na czyms innym niz ta konkretna csvka
 
     # key is tuple of elements
     # value is dict with sid and timestamp
-    # @todo change timestamp to event id,
-
     elements = defaultdict(list)
 
     for index, row in df.iterrows():
         for product in row.Products:
             # @todo change to something faster than list
-            elements[(product,)].append({'sid': row.Id, 'timestamp': row.Date})
+            eid = time.mktime(time.strptime(row.Date, "%Y-%m-%d %H:%M:%S"))
+            elements[((product,),)].append({'sid': row.Id, 'timestamp': row.Date, 'seq':((product,),), 'eid':eid})
 
     freq_single_elements = filter_to_frequent(elements,min_support)
+    freq_double_elements = defaultdict(list)
 
+    # get candidates len(c)==2
+    doublets = candidate_sequences_size_2_but_faster(freq_single_elements, min_support)
+
+    # get freq sets
+    for two_seq in doublets:
+
+        R = join(freq_single_elements[(two_seq[0],)],freq_single_elements[(two_seq[1],)])
+
+        for seq,element in R.items():
+            support = len(set([event["sid"] for event in element]))
+            if support >= min_support:
+                freq_double_elements[seq].extend(element)
+
+    
+    freq = generate_freq_recursive(freq_double_elements,min_support, max_depth)
+
+    print("Frequent single sets:")
+    for key in freq_single_elements.keys():
+        print(key)
+    print("Frequent pairs:")
+    for key in freq_double_elements.keys():
+        print(key)
+
+    print("Frequent rest:")
+    for key in freq.keys():
+        print(key)
     return
 
 
@@ -29,11 +54,13 @@ def main(argv):
 
     parser.add_argument('--dataset',dest='df_filename',help='Filename of Online Retail Dataset II',required=True)
     parser.add_argument('--support',dest='min_support',type=int,help='Minimum support',required=True)
+    parser.add_argument('--max-depth',dest='max_depth',type=int,help='Maximum lenght',required=True)
     args = parser.parse_args(argv)
-    #MIN_SUPPORT = 2
-    df = parse_online_retail(args.df_filename)
+    
+    # limit for testing
+    df = parse_online_retail(args.df_filename).iloc[:20,:]
 
-    mine(df, args.min_support)
+    mine(df, args.min_support, args.max_depth)
 
 
 if __name__ == "__main__":
--- spade/utils.py
+++ spade/utils.py
@@ -98,3 +98,126 @@ def candidate_sequences_size_2(frequent_elements, min_support):
             if(len(tidlist) > min_support):
                 candidates[candidate] = tidlist
         return candidates
+
+
+class Sequence:
+    def __init__(self, products, sid, timestamp):
+        self.products = products
+        self.sid = sid
+        d = time.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+        self.eid = time.mktime(d)
+
+def candidate_sequences_size_2_but_faster(frequent_elements, min_support):
+    '''Given an IdList of atoms, return a dictionary of two-sequences as keys with
+    the frequency of each two-sequence as the value.
+    '''
+
+    # Given an dictionary of Elements, convert it to a horizontal ID list in order to
+    # count the frequency of each two-sequence of atoms.
+    timeline = {} 
+
+    for name,seq_list in frequent_elements.items():
+        for event in seq_list:
+
+            if event["sid"] not in timeline:
+                 timeline[event["sid"]] = []
+
+            #horizontal_db[event["sid"]].append((name,event["timestamp"]))
+            timeline[event["sid"]].append(Sequence(name, event["sid"], event["timestamp"]))
+
+    # create counts using horizontal_db
+    counts = defaultdict(int)
+    
+    for sid,seq in timeline.items():
+        
+        for event_index_i,event_i in enumerate(seq):
+            for event_index_j,event_j in enumerate(seq[event_index_i+1:]):
+
+                if event_i.eid <= event_j.eid:
+                    two_seq = event_i.products + event_j.products
+                else:
+                    two_seq = event_j.products + event_i.products
+
+                counts[two_seq] += 1
+
+    # this is followed by temporal joins between atoms in pairs, so
+    # include only unique combinations
+    return {tuple(sorted(two_seq)) for two_seq,count in counts.items() if count >= min_support}
+
+def flat_tumple(d):
+    for i in d:
+        yield from [i] if not isinstance(i, tuple) else flat_tumple(i)
+
+def join(element_i ,element_j):
+    
+    join_results = defaultdict(list)
+    
+    for event_index_i,event_i in enumerate(element_i):
+        for event_index_j,event_j in enumerate(element_j):
+
+                sid = event_i["sid"]
+
+                # (xy)
+                if event_i["eid"] > event_j["eid"]:
+                    merged_seq = event_j["seq"] + (event_i["seq"][-1],)
+                    merger_dict = {"sid": sid,"eid": event_i["eid"], 'seq': (merged_seq,)}
+                    join_results[merged_seq].append(merger_dict)
+
+                # (yx)
+                elif event_i["eid"] < event_j["eid"]:
+                    merged_seq = event_i["seq"] + (event_j["seq"][-1],)
+                    merger_dict = {"sid": sid,"eid": event_i["eid"], 'seq': (merged_seq,)}
+                    join_results[merged_seq].append(merger_dict)
+
+                elif event_i["seq"][-1] != event_j["seq"][-1]:
+
+                    merger_dict = {"sid": sid,"eid": event_j["eid"]}
+                    try:
+                        # try sorting if all elements are strings
+                        flatten = tuple(sorted((list(event_i["seq"][-1]) + list(event_j["seq"][-1]))))
+                    except:
+                        # fails means nested tuples
+                        tmp = tuple(flat_tumple(tuple((list(event_i["seq"][-1]) + list(event_j["seq"][-1])))))
+                        flatten = tuple(sorted(list(tmp)))
+                    merged_i = event_i["seq"][:-1] + flatten
+
+                    merger_dict["seq"] = (merged_i,)
+                    join_results[(merged_i,)].append(merger_dict)
+
+
+                    merged_j = event_j["seq"][:-1] + flatten
+
+                    # if both resulting atoms are identical, only add it once
+                    if merged_j != merged_i:
+                        merger_dict["seq"] = (merged_j,)
+                        join_results[(merged_j,)].append(merger_dict)
+                
+    return join_results
+
+
+def generate_freq_recursive(elements,min_support, max_depth = 5):
+
+    frequent_elements = defaultdict(list)
+
+    for element_index_i,seq_i in enumerate(elements.keys()):
+
+        frequent_elements_inner = defaultdict(list)
+            
+        for element_index_j,seq_j in enumerate(list(elements.keys())[element_index_i+1:]):
+            if len(tuple(flat_tumple(seq_i + seq_j))) > max_depth +1:
+                continue
+            R = join(elements[seq_i],elements[seq_j])
+
+            for seq,element in R.items():
+                support = len(set([event["sid"] for event in element]))
+                if support >= min_support:
+                    frequent_elements_inner[seq].extend(element)
+
+
+        for seq,element in frequent_elements_inner.items():
+            frequent_elements[seq].extend(element)
+
+        for seq,element in generate_freq_recursive(frequent_elements_inner,min_support, max_depth).items():
+            frequent_elements[seq].extend(element)
+
+    return frequent_elements
